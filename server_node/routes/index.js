1459fb1a1cf4906789f1a48ce55fc2ec7d5f84a2
HyeonGi Min <hyeoni1995@gmail.com>
2019-08-31 17:21:11 +0900

    view setting

--- server_node/routes/index.js
+++ server_node/routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var mysql=require('mysql');
+
+const client=mysql.createConnection({
+  host:'http://hyeoni1995.synology.me',
+  port:3307,
+  user:'bowlAM',
+  password:'Aquea6725!',
+  database:"bowl"
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  /*client.query('select devID,manu,nickname from device_info(error,result)=>{
+    if(error){
+      res.render('error')
+    }else if(!result.length){
+      res.render('mainPage',{data:result,name:name})
+    }
+    else{res.render('mainPage',{data:result,name:name})
+    }
+  })
+  */
+
   res.render('index', { title: 'Express' });
 });
 
